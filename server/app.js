20737894f40cb2719bef67045aaf2cb1501ca5f9
Robert-caridad <robertcaridad@gmail.com>
2024-08-12 16:43:24 +0200

    routes added to the server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
+
 const app = express();
 
 
@@ -11,6 +14,13 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.get('/api/cohorts', (req, res) => {
+  res.json(cohorts)
+})
+
+app.get('/api/students', (req, res) => {
+  res.json(students)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
