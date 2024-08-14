2c4b4ad4c21f60dcdd6d59db25320d184ee74350
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-08-14 19:57:02 +0200

    Added cohorts Routes

---
+++ server/routes/cohorts.routes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router()
+
+const Cohort = require('../models/Cohort.model')
+
+router.post('/', (req, res) => {
+
+  const { inProgress, cohortSlug, cohortName, program, campus, tartDate, endDate, programManager, leadTeacher, totalHours } = req.body
+
+  Cohort
+    .create({ inProgress, cohortSlug, cohortName, program, campus, tartDate, endDate, programManager, leadTeacher, totalHours })
+    .then((newCohort) => res.json(newCohort))
+    .catch((err) => res.status(500).json({ code: 500, message: "Error while creating the cohort", details: err }))
+
+})
+
+router.get('/', (req, res) => {
+
+  Cohort
+    .find
+    .then((cohorts) => res.json(cohorts))
+    .catch((err) => res.status(500).json({ code: 500, message: "Error while fetching the cohorts", details: err }))
+
+})
+
+router.get('/:cohortId', (req, res) => {
+
+  const { cohortId } = req.params
+
+  Cohort
+    .findById(cohortId)
+    .then((cohorts) => res.json(cohorts))
+    .catch((err) => res.status(500).json({ code: 500, message: "Error while fetching the cohorts", details: err }))
+
+})
+
+router.put('/:cohortId', (req, res) => {
+
+  const { inProgress, cohortSlug, cohortName, program, campus, tartDate, endDate, programManager, leadTeacher, totalHours } = req.body
+  const { cohortId } = req.params
+
+  Cohort
+    .findByIdAndUpdate(cohortId, { inProgress, cohortSlug, cohortName, program, campus, tartDate, endDate, programManager, leadTeacher, totalHours }, { new: true })
+    .then((cohort) => res.json(cohort))
+    .catch((err) => res.status(500).json({ code: 500, message: "Error while editing cohort", details: err }))
+
+})
+
+router.delete('/:cohortId', (req, res) => {
+
+  const { cohortId } = req.params
+
+  Cohort
+    .findByIdAndDelete(cohortId)
+    .then((cohort) => res.json(cohort))
+    .catch((err) => res.status(500).json({ code: 500, message: "Error while deleting cohort", details: err }))
+
+})
+
+module.exports = router
