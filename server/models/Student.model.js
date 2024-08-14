279a19d40f11873a4ce7beafbdbee2d0169f8f0b
Robert-caridad <robertcaridad@gmail.com>
2024-08-14 15:53:35 +0200

    added student model

---
+++ server/models/Student.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mogoose")
+const { Schema, module } = mongoose
+
+const studentSchema = new Schema({
+
+    "firstName": {
+        type: String,
+        required: true
+    },
+    "lastName": {
+        type: String,
+        required: true
+    },
+    "email": {
+        type: String,
+        required: true,
+        unique: true
+    },
+    "phone": {
+        type: String,
+        required: true
+    },
+    "linkedinUrl": {
+        type: String,
+        default: ""
+    },
+    "languages": {
+        type: [""]
+    },
+    "program": {
+        type: String,
+        enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]
+    },
+    "background": {
+        type: String,
+        default: ""
+    },
+    "image": {
+        type: String,
+        default: "https://i.imgur.com/r8bo8u7.png"
+    },
+    "cohort": {
+        type: Schema.Types.ObjectId,
+        ref: "Cohort"
+    },
+    "projects": {
+        type: Array,
+        default: []
+    },
+},
+    {
+        Timestamp: true
+    }
+)
+
+
+const Student = model('Student', studentSchema)
+
+module.export = Student
